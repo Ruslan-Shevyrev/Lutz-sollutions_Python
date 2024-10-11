70cf314f3ee52c0eab43d4afc7e5c4f848dbf2f8
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-10-11 13:39:30 +0300

    hierarchy_animal

---
+++ hierarchy_animal.py
@@ -0,0 +1,29 @@
+class Animal:
+    def reply(self):
+        self.speak()
+
+    def speak(self):
+        print('Hello world!')
+
+
+class Mammal(Animal):
+    def speak(self):
+        print('Milk')
+
+
+class Dog(Mammal):
+    def speak(self):
+        print('Gav')
+
+
+class Cat(Mammal):
+    def speak(self):
+        print('Meow')
+
+class Primate(Mammal):
+    def speak(self):
+        print('Where I am?')
+
+
+class Hacker(Primate):
+    pass
