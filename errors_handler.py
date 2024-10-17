3c0156fa7b88951c19545dca7d26a6f4846103b2
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-10-17 17:58:54 +0300

    errors_handler

---
+++ errors_handler.py
@@ -0,0 +1,15 @@
+import sys
+import traceback
+
+
+def safe(callee, *pargs, **kargs):
+    try:
+        callee(*pargs, **kargs)
+    except:
+        traceback.print_exc()
+        print(str(sys.exc_info()[0]) + ' ' + str(sys.exc_info()[1]))
+
+
+if __name__ == '__main__':
+    import try_except
+    safe(try_except.oops)
