0813a4a7f055c1773c898ee606e9f915fc733b3f
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2025-03-13 11:41:17 +0300

    add manager

---
+++ employees_db/manager.py
@@ -0,0 +1,14 @@
+from person import Person
+
+
+class Manager(Person):
+    def give_raise(self, percent: float, bonus: float = 0.1):
+        Person.give_raise(self, percent + bonus)
+
+
+if __name__ == '__main__':
+    tom = Manager('Tom Doe', 50, 50000, 'software engineer')
+    print(tom.last_name())
+    print('Tom\'s pay before raise: ' + str(tom.pay))
+    tom.give_raise(.10)
+    print('Tom\'s pay after raise: ' + str(tom.pay))
