3f0131b7c56c3fe95b9bb532eb23b99d86da3734
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2025-03-24 09:31:05 +0300

    add lister_walk

---
+++ os/lister_walk.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+PATH = 'C:\Test'
+
+
+def lister(root):
+    for (this_dir, subs_here, files_here) in os.walk(root):
+        print('['+this_dir+']')
+        for f_name in files_here:
+            path = os.path.join(this_dir, f_name)
+            print(path)
+
+
+if __name__ =='__main__':
+    lister(PATH)
