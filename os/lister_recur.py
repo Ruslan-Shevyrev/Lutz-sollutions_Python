9711440bdaaf75204bef735074d7dd9042aaf21e
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2025-03-24 09:35:46 +0300

    add lister_recur

--- os/lister_recur.py
+++ os/lister_recur.py
@@ -3,15 +3,15 @@ import os
 PATH = 'C:\Test'
 
 
-def rec_lister(curr_dir):
+def lister(curr_dir):
     print('[' + curr_dir + ']')
     for file in os.listdir(curr_dir):
         path = os.path.join(curr_dir, file)
         if not os.path.isdir(path):
             print(path)
         else:
-            rec_lister(path)
+            lister(path)
 
 
 if __name__ =='__main__':
-    rec_lister(PATH)
+    lister(PATH)
