796b88018950be4acf2af2124bb4a49acc2fac11
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2025-03-27 15:27:20 +0300

    add searc_files

---
+++ biggest_and_smallest_file/main.py
@@ -0,0 +1,5 @@
+import search_file
+
+DIR_NAME = r'C:\Users\rshevyrev\PycharmProjects\Lutz_sollutions'
+
+search_file.search_in_one_dir(DIR_NAME, 3)
---
+++ biggest_and_smallest_file/search_file.py
@@ -0,0 +1,13 @@
+import os
+import glob
+
+
+def search_in_one_dir(dir_name, count):
+    all_sizes = []
+    all_py = glob.glob(dir_name + os.sep + '*.py')
+    for file_name in all_py:
+        file_size = os.path.getsize(file_name)
+        all_sizes.append((file_size, file_name))
+    all_sizes.sort()
+    print(all_sizes[:count])
+    print(all_sizes[-count:])
