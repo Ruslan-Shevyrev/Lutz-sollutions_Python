55612a7d41bc2a39af2d2effb0751449cc918e63
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2025-04-18 14:24:04 +0300

    add set_bg_color

--- gui/set_color/main.py
+++ gui/set_color/main.py
@@ -9,8 +9,13 @@ def set_bg_color():
         push.config(bg=hex_str)
 
 
+def on_left_click(event):
+    print('Left Click')
+
+
 root = Tk()
 push = Button(root, text='Set bg color', command=set_bg_color)
 push.config(height=3, font=('times', 20, 'bold'))
 push.pack(expand=YES, fill=BOTH)
+root.bind('<Button-1>', on_left_click)
 root.mainloop()
